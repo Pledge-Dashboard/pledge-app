cc000944d023ee75c9635bc87f82b25e6a1ea693
Rakshit <rakshit087@gmail.com>
2022-11-05 17:11:53 +0530

    refactor(NavLink): seprate component

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex, Text, chakra, Image, Spacer, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { NavLink } from '../components/NavLink';
 import { SideDrawer } from './SideDrawer';
 
 export const Navbar = () => {
@@ -67,41 +66,5 @@ export const Navbar = () => {
         </Box>
       </chakra.nav>
     </chakra.header>
-  );
-};
-
-const NavLink = ({ href, displayString }: { href: string; displayString: string }) => {
-  const router = useRouter();
-  const isActive = router.asPath == href ? true : false;
-
-  const [width, setWidth] = useState('0');
-
-  useEffect(() => setWidth(router.asPath == href ? '3ch' : '0'), [href, router]);
-
-  return (
-    <NextLink
-      legacyBehavior
-      href={href}
-      passHref
-      scroll={false}
-    >
-      <Link
-        _hover={{
-          _after: {
-            width: width !== '0' ? '3ch' : '1.5ch',
-          },
-        }}
-        _after={{
-          content: '""',
-          width: width,
-          height: '2px',
-          display: 'block',
-          bg: 'bg.gradient',
-          transition: 'width 0.3s ease-in-out',
-        }}
-      >
-        {displayString}
-      </Link>
-    </NextLink>
   );
 };
