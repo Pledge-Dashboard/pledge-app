3d0d67bba94d870418104326aa51423ddc005637
Rakshit <rakshit087@gmail.com>
2022-11-06 20:40:18 +0530

    refactor(landing-section): Seprate component for landing section

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,51 +7,41 @@ import Staderprice from '../data/stader/staderapi';
 import Ankrbprice from '../data/ankr/abankrprice';
 import Ankrcprice from '../data/ankr/acankrprice';
 import Tenderizeprice from '../data/tenderize/tenderizeprice';
+import { Landing } from '../sections/Landing';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      {/* <Component {...pageProps} /> */}
+      <Landing />
       <Box
-        mt={24}
-        scrollBehavior="smooth"
+        h="95vh"
+        id="Statistics"
+        as="section"
+        w="full"
+        bg="green.200"
       >
-        {/* <Component {...pageProps} /> */}
-        <Box
-          h="95vh"
-          id="Landing"
-          as="section"
-          w="full"
-          bg="green.100"
-        >
-          <Lidoprice />
-          <Staderprice />
-          <Ankrbprice />
-          <Ankrcprice />
-          <Tenderizeprice />
-        </Box>
-        <Box
-          h="95vh"
-          id="Statistics"
-          as="section"
-          w="full"
-          bg="green.200"
-        />
-        <Box
-          h="95vh"
-          id="LiquidStaking"
-          as="section"
-          w="full"
-          bg="green.300"
-        />
-        <Box
-          h="95vh"
-          id="Platforms"
-          as="section"
-          w="full"
-          bg="green.400"
-        />
+        <Lidoprice />
+        <Staderprice />
+        <Ankrbprice />
+        <Ankrcprice />
+        <Tenderizeprice />
       </Box>
+      <Box
+        h="95vh"
+        id="LiquidStaking"
+        as="section"
+        w="full"
+        bg="green.300"
+      />
+      <Box
+        h="95vh"
+        id="Platforms"
+        as="section"
+        w="full"
+        bg="green.400"
+      />
     </ChakraProvider>
   );
 }
---
+++ src/sections/Landing.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export const Landing = () => {
+  return (
+    <Flex
+      h="100vh"
+      id="Landing"
+      as="section"
+      w="full"
+      alignItems={'center'}
+      justifyContent={'center'}
+    >
+      <Text>Pledge</Text>
+    </Flex>
+  );
+};
