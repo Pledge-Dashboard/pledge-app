6bdf4913c81a97e388485d0f2e9a1a6359a7da41
jayeshbhole <jayeshbhole123@gmail.com>
2022-11-07 23:10:55 +0530

    feat: display blurCircle

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text } from '@chakra-ui/react';
+import BlurCircle from '../components/BlurCircle';
 
 export const Landing = () => {
   return (
@@ -11,6 +12,15 @@ export const Landing = () => {
       justifyContent={'center'}
       flexDir={'column'}
     >
+      <BlurCircle
+        blur={80}
+        width="35rem"
+        opacity="0.2"
+        position="absolute"
+        top="0"
+        right="0"
+        transform="translate(25%, -50%)"
+      />
       <Text
         fontSize={{ base: '5xl', md: '6xl', lg: '7xl' }}
         fontWeight={500}
