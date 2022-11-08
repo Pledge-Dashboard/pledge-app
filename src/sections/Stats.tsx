e69d0e0cf0d7a5a88e2a91069412996cb47bb717
Rakshit <rakshit087@gmail.com>
2022-11-08 22:56:26 +0530

    feat(stats.tsx): Dummy widgets

--- src/sections/Stats.tsx
+++ src/sections/Stats.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { Flex, Text, Tabs, TabList, TabPanels, Tab, TabPanel, Box } from '@chakra-ui/react';
 
 export const Stats = () => {
   return (
@@ -16,23 +16,74 @@ export const Stats = () => {
         fontWeight={500}
         bg="bg.gradient"
         backgroundClip="text"
+        mb={12}
       >
         stats!
       </Text>
-      {/* <Tabs variant="unstyled">
-        <TabList>
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
+      <Tabs variant="unstyled">
+        <TabList gap={12}>
+          {['Stader', 'Lido', 'Claystack', 'Ankr', 'Tenderize'].map((tab, index) => (
+            <Tab
+              _selected={{ color: 'white', bg: '#D9D9D91A' }}
+              borderRadius="12"
+              px={6}
+              key={index}
+            >
+              {tab}
+            </Tab>
+          ))}
         </TabList>
         <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
+          {['Stader', 'Lido', 'Claystack', 'Ankr', 'Tenderize'].map((tab, index) => (
+            <TabPanel
+              key={index}
+              px={0}
+            >
+              <Flex
+                w="full"
+                align={'center'}
+                py={8}
+                px={8}
+                justifyContent={'space-between'}
+              >
+                <Flex
+                  flexDir={'column'}
+                  alignItems={'center'}
+                >
+                  <Box
+                    bg="#D9D9D91A"
+                    w={32}
+                    h={32}
+                    borderRadius={'full'}
+                    mb={8}
+                  />
+                  <Box
+                    bg="#D9D9D91A"
+                    w={48}
+                    h={32}
+                    borderRadius={'12'}
+                  />
+                </Flex>
+                <Box>
+                  <Box
+                    bg="#D9D9D91A"
+                    w={72}
+                    h={12}
+                    borderRadius={'12'}
+                    mb={8}
+                  />
+                  <Box
+                    bg="#D9D9D91A"
+                    w={72}
+                    h={56}
+                    borderRadius={'12'}
+                  />
+                </Box>
+              </Flex>
+            </TabPanel>
+          ))}
         </TabPanels>
-      </Tabs> */}
+      </Tabs>
     </Flex>
   );
 };
