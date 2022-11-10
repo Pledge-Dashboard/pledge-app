96b376c2321c52fe63b83b9c3b6bb1e3de8a36f3
Rakshit <rakshit087@gmail.com>
2022-11-10 15:15:06 +0530

    refactor(Landing.tsx): commented out blur circle for now

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,4 +1,4 @@
-import { Flex, Spacer, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import BlurCircle from '../components/BlurCircle';
 import { LandingWidget } from '../components/LandingWidget';
 
@@ -13,16 +13,16 @@ export const Landing = () => {
       justifyContent={'center'}
       flexDir={'column'}
     >
-      <BlurCircle
+      {/* <BlurCircle
         blur={80}
-        width="35rem"
+        width="35%"
         opacity="0.2"
         position="absolute"
         top="0"
         right="0"
         transform="translate(25%, -50%)"
         display={{ base: 'none', lg: 'block' }}
-      />
+      /> */}
       <Text
         fontSize={{ base: '5xl', md: '6xl', lg: '7xl' }}
         fontWeight={500}
