5e956575e494038c2af6ee5f8b1cbd90fbb2f307
Rakshit <rakshit087@gmail.com>
2022-11-12 00:55:10 +0530

    feat(api/dummy): dummiest dummy api

---
+++ src/pages/Api/dummy.ts
@@ -0,0 +1,71 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const data = {
+  totalStacked: '21.5 M',
+  totalDerivatives: '5',
+  averageAPY: '12.5%',
+  derivatives: ['Stader', 'Lido', 'Claystack', 'Ankr', 'Tenderize'],
+  stats: {
+    stader: {
+      logo: 'https://staderlabs.com/stader-logo.svg?imwidth=2048',
+      token: 'MATICx',
+      apy: '5.6%',
+      tokenPrice: '0.98',
+      tvl: '41.1 M',
+      growth: {
+        day: '1.2%',
+        week: '3.4%',
+        month: '5.6%',
+      },
+    },
+    lido: {
+      logo: 'https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/modified/0f05fd8b-d9b5-4105-8ae5-87ff9d4c6fb2.png',
+      token: 'stMATIC',
+      apy: '6.3%',
+      tokenPrice: '0.98',
+      tvl: '53.2 M',
+      growth: {
+        day: '1.2%',
+        week: '3.4%',
+        month: '5.6%',
+      },
+    },
+    claystack: {
+      token: 'MATICx',
+      apy: '5.6%',
+      tokenPrice: '0.98',
+      tvl: '41.1 M',
+      growth: {
+        day: '1.2%',
+        week: '3.4%',
+        month: '5.6%',
+      },
+    },
+    ankr: {
+      token: 'MATICx',
+      apy: '5.6%',
+      tokenPrice: '0.98',
+      tvl: '41.1 M',
+      growth: {
+        day: '1.2%',
+        week: '3.4%',
+        month: '5.6%',
+      },
+    },
+    tenderize: {
+      token: 'MATICx',
+      apy: '5.6%',
+      tokenPrice: '0.98',
+      tvl: '41.1 M',
+      growth: {
+        day: '1.2%',
+        week: '3.4%',
+        month: '5.6%',
+      },
+    },
+  },
+};
+
+export const handler = (_req: NextApiRequest, _res: NextApiResponse) => {
+  _res.status(200).json(data);
+};
