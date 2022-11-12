5e1f53340797f22a9c34e5d1f94ab107169f9d7b
Rakshit <rakshit087@gmail.com>
2022-11-13 01:44:17 +0530

    feat(Here.tsx): added Grid to here section

--- src/sections/Here.tsx
+++ src/sections/Here.tsx
@@ -1,4 +1,5 @@
 import { SectionContainer } from '../layouts/SectionContainer';
+import { Text, Grid, GridItem } from '@chakra-ui/react';
 
 export const Here = () => {
   return (
@@ -6,7 +7,55 @@ export const Here = () => {
       id="here"
       title="here!"
     >
-      <></>
+      <>
+        <Text
+          fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}
+          mt={6}
+        >
+          Stake your MATIC
+        </Text>
+        {/*Create a responsive grid with fixed width cards*/}
+        <Grid
+          mt={12}
+          templateColumns={{
+            base: 'repeat(1, 1fr)',
+            md: 'repeat(2, 1fr)',
+            lg: 'repeat(3, 1fr)',
+          }}
+          gap={12}
+        >
+          <GridItem
+            w="16rem"
+            h="12rem"
+            bg="bg.translucent"
+            borderRadius="xl"
+          />
+          <GridItem
+            w="16rem"
+            h="12rem"
+            bg="bg.translucent"
+            borderRadius="xl"
+          />
+          <GridItem
+            w="16rem"
+            h="12rem"
+            bg="bg.translucent"
+            borderRadius="xl"
+          />
+          <GridItem
+            w="16rem"
+            h="12rem"
+            bg="bg.translucent"
+            borderRadius="xl"
+          />
+          <GridItem
+            w="16rem"
+            h="12rem"
+            bg="bg.translucent"
+            borderRadius="xl"
+          />
+        </Grid>
+      </>
     </SectionContainer>
   );
 };
