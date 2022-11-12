8e3a4aa47683132be92a2cf5a8d49388cc4874bb
Rakshit <rakshit087@gmail.com>
2022-11-13 01:15:07 +0530

    refactor!: seprated _app and index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,13 @@
 import type { AppProps } from 'next/app';
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Navbar } from '../layouts/Navbar';
 import theme from '../theme';
-import Lidoprice from '../data/lidoapi/lidoprice';
-import Staderprice from '../data/stader/staderapi';
-import Ankrbprice from '../data/ankr/abankrprice';
-import Ankrcprice from '../data/ankr/acankrprice';
-import Tenderizeprice from '../data/tenderize/tenderizeprice';
-import { Landing } from '../sections/Landing';
-import { Stats } from '../sections/Stats';
-import { Here } from '../sections/Here';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
-      {/* <Component {...pageProps} /> */}
-      <Landing />
-      <Stats />
-      <Here />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
