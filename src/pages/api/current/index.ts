9842d87e5b0c90cdb93547d229e02ebf16a14e75
jayeshbhole <jayeshbhole123@gmail.com>
2022-11-13 14:39:17 +0530

    Squashed commit of the following:
    commit 1324bbecba349cbaf5552fd971b5988e909a269d
    Author: jayeshbhole <jayeshbhole123@gmail.com>
    Date:   Sun Nov 13 14:38:58 2022 +0530
    
        Delete package-lock.json
    
    commit dbc9acc6e8e0f8713f92b5ed18ea02925dd20dde
    Author: jayeshbhole <jayeshbhole123@gmail.com>
    Date:   Sun Nov 13 14:29:53 2022 +0530
    
        fix: add error handler

--- src/pages/api/current/index.ts
+++ src/pages/api/current/index.ts
@@ -3,12 +3,17 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../../../lib/mongodb';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  const { database }: { database: Db } = await connectToDatabase();
-  const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION || '');
+  try {
+    const { database }: { database: Db } = await connectToDatabase();
+    const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION || '');
 
-  //   fetch latest document from collection
-  const results = await collection.findOne({}, { sort: { timestamp: -1 } });
+    //   fetch latest document from collection
+    const results = await collection.findOne({}, { sort: { timestamp: -1 } });
 
-  response.setHeader('Cache-Control', 's-maxage=300');
-  response.status(200).json(results);
+    response.setHeader('Cache-Control', 's-maxage=300');
+    response.status(200).json(results);
+  } catch (e) {
+    console.log(e);
+    response.status(500).json({ error: e });
+  }
 }
