17af1b1a593a148e65bd8e1f1f95f63a1ef93460
Rakshit <rakshit087@gmail.com>
2022-11-15 00:33:10 +0530

    feat(LiquidStackingInfo): added cards

--- src/sections/LiquidStakingInfo.tsx
+++ src/sections/LiquidStakingInfo.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { SectionContainer } from '../layouts/SectionContainer';
 
 export const LiquidStakingInfo = () => {
@@ -6,7 +7,63 @@ export const LiquidStakingInfo = () => {
       id="LiquidStaking"
       title="what?"
     >
-      <></>
+      <Flex
+        grow={'1'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        gap={{ base: '6', md: '8' }}
+        flexDir={{ base: 'column', md: 'row' }}
+        mt={{ base: '8', md: '0' }}
+      >
+        <Box
+          bgColor={'bg.translucent'}
+          py={8}
+          px={4}
+          borderRadius={'xl'}
+          boxShadow={'xl'}
+          width={'xs'}
+        >
+          <Text
+            fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}
+            fontWeight="bold"
+          >
+            Deposit Your Tokens
+          </Text>
+          <Text fontSize={{ base: 'sm', md: 'md', lg: 'md' }}> on any liquid stacking platform of your choice </Text>
+        </Box>
+        <Box
+          bgColor={'bg.translucent'}
+          py={8}
+          px={4}
+          borderRadius={'xl'}
+          boxShadow={'xl'}
+          width={'xs'}
+        >
+          <Text
+            fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}
+            fontWeight="bold"
+          >
+            Deposit Your Tokens
+          </Text>
+          <Text fontSize={{ base: 'sm', md: 'md', lg: 'md' }}> on any liquid stacking platform of your choice </Text>
+        </Box>
+        <Box
+          bgColor={'bg.translucent'}
+          py={8}
+          px={4}
+          borderRadius={'xl'}
+          boxShadow={'xl'}
+          width={'xs'}
+        >
+          <Text
+            fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}
+            fontWeight="bold"
+          >
+            Deposit Your Tokens
+          </Text>
+          <Text fontSize={{ base: 'sm', md: 'md', lg: 'md' }}> on any liquid stacking platform of your choice </Text>
+        </Box>
+      </Flex>
     </SectionContainer>
   );
 };
