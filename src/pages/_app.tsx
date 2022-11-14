bc9e07abf98d9fbc44178f9f0329e541776b3326
jayeshbhole <jayeshbhole123@gmail.com>
2022-11-15 00:07:37 +0530

    chore: remove parallax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,37 +7,12 @@ import { Landing } from '../sections/Landing';
 import { Stats } from '../sections/Stats';
 import Platforms from '../sections/Platforms';
 import LiquidStakingInfo from '../sections/LiquidStakingInfo';
-import BlurCircle from '../components/BlurCircle';
-import { useRef } from 'react';
-import { motion, useScroll } from 'framer-motion';
-import { useParallax } from '../hooks/useParallax';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const y = useParallax(scrollYProgress, -100);
-
   return (
     <DataStoreProvider>
       <ChakraProvider theme={theme}>
-        <Box ref={ref}>
-          <Box
-            as={motion.div}
-            style={{ y }}
-            position="absolute"
-            w="75%"
-            top="-10%"
-            right="-10%"
-            mixBlendMode={'color'}
-          >
-            <BlurCircle
-              blur={80}
-              w="100%"
-              pt="100%"
-              opacity={1}
-              // display={{ base: 'none', lg: 'block' }}
-            />
-          </Box>
+        <Box>
           <Navbar />
           {/* <Component {...pageProps} /> */}
           <Landing />
