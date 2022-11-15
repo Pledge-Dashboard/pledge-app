0b209f8e394ae804aea77033dac6292d6e97849f
Rakshit <rakshit087@gmail.com>
2022-11-16 01:06:19 +0530

    refactor(LiquidStakingCard): added card component for stacking info

--- src/sections/LiquidStakingInfo.tsx
+++ src/sections/LiquidStakingInfo.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
+import { StakeInfoCard } from '../components/StakeInfoCard';
 
 export const LiquidStakingInfo = () => {
   return (
@@ -26,93 +27,31 @@ export const LiquidStakingInfo = () => {
         Understand Liquid Staking
       </Text>
       <Flex justifyContent={{ base: 'center', md: 'flex-start' }}>
-        <Flex
-          bgColor={'bg.translucent'}
-          textAlign={{ base: 'center', md: 'left' }}
-          flexDirection={'column'}
-          alignItems={'flex-end'}
-          w={{ base: 'full', md: '75%', lg: '60%' }}
-          minH={'8rem'}
-          mb={{ base: 8, md: 12, lg: 4 }}
-        >
-          <Box
-            width={{ base: 'full', md: '75%', lg: '40%' }}
-            pb={{ base: 6 }}
-            pt={{ base: 6, md: 12 }}
-            pl={{ base: 6, lg: 16 }}
-            pr={{ base: 6, lg: 10 }}
-          >
-            <Text
-              fontWeight={'bold'}
-              fontSize={{ base: 'xl', md: '2xl', lg: '3xl' }}
-              mb={4}
-            >
-              Staking?
-            </Text>
-            <Text fontSize={{ base: 'sm', md: 'md' }}>
-              Stake MATIC with validators and participate in growth of the network!
-            </Text>
-          </Box>
-        </Flex>
+        <StakeInfoCard
+          title="Staking?"
+          description="Stake MATIC with validators and participate in growth of the network!"
+          cardWidth={{ base: 'full', md: '75%', lg: '60%' }}
+          textWidth={{ base: 'full', md: '75%', lg: '40%' }}
+          alignment="flex-end"
+        />
       </Flex>
       <Flex justifyContent={{ base: 'center', md: 'flex-end' }}>
-        <Flex
-          bgColor={'bg.translucent'}
-          textAlign={{ base: 'center', md: 'left' }}
-          flexDirection={'column'}
-          alignItems={'flex-start'}
-          w={{ base: 'full', md: '50%', lg: '42.5%' }}
-          minH={'8rem'}
-          mb={{ base: 8, md: 12, lg: 4 }}
-        >
-          <Box
-            width={{ base: 'full', md: '75%', lg: '50%' }}
-            pb={{ base: 6 }}
-            pt={{ base: 6, md: 12 }}
-            pl={{ base: 6, lg: 16 }}
-            pr={{ base: 6, lg: 10 }}
-          >
-            <Text
-              fontWeight={'bold'}
-              fontSize={{ base: 'xl', md: '2xl', lg: '3xl' }}
-              mb={4}
-            >
-              Returns!
-            </Text>
-            <Text fontSize={{ base: 'sm', md: 'md' }}>Get returns for staked MATIC!!</Text>{' '}
-            <Text fontSize={{ base: 'sm', md: 'md' }}>Current Avg. APY: __%</Text>
-          </Box>
-        </Flex>
+        <StakeInfoCard
+          title="Returns!"
+          description="Get returns for staked MATIC!! Current Avg. APY: __%"
+          cardWidth={{ base: 'full', md: '50%', lg: '42.5%' }}
+          textWidth={{ base: 'full', md: '75%', lg: '50%' }}
+          alignment="flex-start"
+        />
       </Flex>
       <Flex justifyContent={{ base: 'center', md: 'flex-start' }}>
-        <Flex
-          bgColor={'bg.translucent'}
-          textAlign={{ base: 'center', md: 'left' }}
-          flexDirection={'column'}
-          alignItems={'flex-end'}
-          w={{ base: 'full', md: '60%', lg: '50%' }}
-          minH={'8rem'}
-          mb={{ base: 8, md: 12, lg: 4 }}
-        >
-          <Box
-            width={{ base: 'full', md: '75%', lg: '50%' }}
-            pb={{ base: 6 }}
-            pt={{ base: 6, md: 12 }}
-            pl={{ base: 6, lg: 16 }}
-            pr={{ base: 6, lg: 10 }}
-          >
-            <Text
-              fontWeight={'bold'}
-              fontSize={{ base: 'xl', md: '2xl', lg: '3xl' }}
-              mb={4}
-            >
-              Liquid?!
-            </Text>
-            <Text fontSize={{ base: 'sm', md: 'md' }}>
-              Trade, invest and use receipt tokens (stMATIC, tMATIC, aMATICb, etc.)!!!
-            </Text>
-          </Box>
-        </Flex>
+        <StakeInfoCard
+          title="Liquid?"
+          description=" Trade, invest and use receipt tokens (stMATIC, tMATIC, aMATICb, etc.)!!!"
+          cardWidth={{ base: 'full', md: '60%', lg: '50%' }}
+          textWidth={{ base: 'full', md: '75%', lg: '50%' }}
+          alignment="flex-end"
+        />
       </Flex>
     </Box>
   );
