e500af8f5e29a7d759cb7c35e2360393f44a42a1
jayeshbhole <jayeshbhole123@gmail.com>
2022-11-18 19:21:39 +0530

    Squashed commit of the following:
    commit 12791ac2460bd7557947d57ac882f0125a97e88f
    Author: jayeshbhole <jayeshbhole123@gmail.com>
    Date:   Fri Nov 18 18:43:07 2022 +0530
    
        fix: info cell paddings
    
    commit b52933a756657ddc4091f8c13a94a3d438046be5
    Author: jayeshbhole <jayeshbhole123@gmail.com>
    Date:   Fri Nov 18 18:31:36 2022 +0530
    
        style: fix doodle display in mobile, chart margins
    
    commit 73f13b2be15035e6ad8b757ca5d8d79e3374c148
    Author: jayeshbhole <jayeshbhole123@gmail.com>
    Date:   Fri Nov 18 18:19:47 2022 +0530
    
        fix: responsive styles
    
    commit 51ea1434f7bf64995464d392d2983c423b0b8ba4
    Author: jayeshbhole <jayeshbhole123@gmail.com>
    Date:   Fri Nov 18 17:51:31 2022 +0530
    
        fix: url

--- src/components/CssDoodle.tsx
+++ src/components/CssDoodle.tsx
@@ -5,6 +5,7 @@ import 'css-doodle';
 const CssDoodle: FC<ChakraProps & { doodle: string }> = ({ doodle, ...props }) => {
   return (
     <Box
+      display={{ base: 'none', md: 'block' }}
       dangerouslySetInnerHTML={{
         __html: doodle,
       }}
