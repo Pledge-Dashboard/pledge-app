fd18706cb33cfb43675183afd4c7d545649e9c06
Rakshit <rakshit087@gmail.com>
2022-11-19 23:04:24 +0530

    fix(Platform): grid size 4x1 for large screens

--- src/sections/Platforms.tsx
+++ src/sections/Platforms.tsx
@@ -47,7 +47,7 @@ const Platforms = () => {
           templateColumns={{
             base: 'repeat(1, 1fr)',
             md: 'repeat(2, 1fr)',
-            lg: 'repeat(3, 1fr)',
+            lg: 'repeat(4, 1fr)',
           }}
           gap={12}
         >
